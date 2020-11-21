aedaab36bcca25ee51c52b2c81d851ef81b53aed
Willems Davy <joyrider3774@hotmail.com>
2020-11-21 05:31:43 +0100

    Initial Version

---
+++ GameImportView.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Playnite.SDK.Models;
+using System.Windows;
+using System.Windows.Controls;
+using Playnite.SDK;
+
+namespace SimpleGameImport
+{
+    public partial class GameImportView: UserControl
+    {
+        private readonly SimpleGameImport plugin;
+        private static IResourceProvider resources = new ResourceProvider();
+        public List<string> PlatformsList { get; set; } = new List<string>();
+        public List<string> SourcesList { get; set; } = new List<string>();
+        
+        public GameImportView()
+        {
+            InitializeComponent();
+        }
+        public GameImportView(SimpleGameImport plugin)
+        {
+            this.plugin = plugin;            
+            var TmpPlatformsList = plugin.PlayniteApi.Database.Platforms.AsQueryable().OrderBy(o => o.Name).ToList();
+            foreach (Platform platform in TmpPlatformsList)
+            {
+                PlatformsList.Add(platform.Name);
+            }
+            var TmpSourcesList = plugin.PlayniteApi.Database.Sources.AsQueryable().OrderBy(o => o.Name).ToList();
+            SourcesList.Add("");
+            foreach (GameSource source in TmpSourcesList)
+            {
+                SourcesList.Add(source.Name);
+            }
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int NumGamesAdded = 0;
+            int NumGamesNotAddedDouble = 0;
+            if (tbGameNames.Text.Trim() == "")
+            {
+                plugin.PlayniteApi.Dialogs.ShowMessage(resources.GetString("LOC_SIMPLEGAMEIMPORTER_NoGames"));
+                return;
+            }
+            if (CmbPlatforms.SelectedItem == null)
+            {
+                plugin.PlayniteApi.Dialogs.ShowMessage(resources.GetString("LOC_SIMPLEGAMEIMPORTER_NoPlatform"));
+                return;
+            }
+            var gamesText = tbGameNames.Text.Replace("\r\n", "\n");
+            var games = gamesText.Split('\n');
+            foreach (string game in games)
+            {
+                if ((game.Trim() != ""))
+                {
+                    bool Exists = false;
+                    var GameSearch = plugin.PlayniteApi.Database.Games.Where(o => o.Name == game);
+                    if (GameSearch.HasItems())
+                    {
+                        foreach(Game PlayniteGame in GameSearch)
+                        {
+                            if (PlayniteGame.Platform.Name == CmbPlatforms.SelectedItem.ToString())
+                            {
+                                Exists = true;
+                                NumGamesNotAddedDouble++;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (!Exists)
+                    {
+                        var newGame = new Game(game);
+                        var platformList = plugin.PlayniteApi.Database.Platforms.Where(o => o.Name == CmbPlatforms.SelectedItem.ToString());
+                        if (platformList.HasItems())
+                        {
+                            newGame.PlatformId = platformList.First().Id;
+                        }
+                        if (CmbSources.SelectedItem != null && (CmbSources.SelectedItem.ToString() != ""))
+                        {
+                            var sourceList = plugin.PlayniteApi.Database.Sources.Where(o => o.Name == CmbSources.SelectedItem.ToString());
+                            if (sourceList.HasItems())
+                            {
+                                newGame.SourceId = sourceList.First().Id;
+                            }
+                        }
+                        newGame.Added = DateTime.Now;
+                        plugin.PlayniteApi.Database.Games.Add(newGame);
+                        NumGamesAdded++;
+                    }
+                }                
+            }
+            plugin.PlayniteApi.Dialogs.ShowMessage(resources.GetString("LOC_SIMPLEGAMEIMPORTER_GamesAdded") + $": {NumGamesAdded}\n" +
+                resources.GetString("LOC_SIMPLEGAMEIMPORTER_GamesNotAdded") +  $": {NumGamesNotAddedDouble} ");
+        }
+    }
+}
---
+++ SimpleGameImport.cs
@@ -0,0 +1,124 @@
+﻿using Playnite.SDK;
+using Playnite.SDK.Models;
+using Playnite.SDK.Plugins;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SimpleGameImport
+{
+    public class SimpleGameImport : Plugin
+    {
+        private static readonly ILogger logger = LogManager.GetLogger();
+        private static IResourceProvider resources = new ResourceProvider();
+
+        private string pluginFolder;
+
+        private SimpleGameImportSettings Settings { get; set; }
+
+        public override Guid Id { get; } = Guid.Parse("6048b590-bdfb-4a88-8953-2985b4f5c3dc");
+
+        public SimpleGameImport(IPlayniteAPI api) : base(api)
+        {
+            Settings = new SimpleGameImportSettings(this);
+
+            pluginFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            Localization.SetPluginLanguage(pluginFolder, api.ApplicationSettings.Language);
+        }
+
+        public void DoImportGames()
+        {
+            GameImportView ImportView = new GameImportView(this);
+            var window = PlayniteApi.Dialogs.CreateWindow(new WindowCreationOptions
+            {
+                ShowMinimizeButton = false,
+            });
+
+            window.Height = 350;
+            window.Width = 650;
+            window.Title = "Simple Game Importer";
+
+            // Set content of a window. Can be loaded from xaml, loaded from UserControl or created from code behind
+            window.Content = ImportView;
+
+            // Set owner if you need to create modal dialog window
+            window.Owner = PlayniteApi.Dialogs.GetCurrentAppWindow();
+            window.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+
+            // Use Show or ShowDialog to show the window
+            window.ShowDialog();
+        }
+
+        public override List<MainMenuItem> GetMainMenuItems(GetMainMenuItemsArgs args)
+        {
+            List<MainMenuItem> MainMenuItems = new List<MainMenuItem>
+            {
+                new MainMenuItem {
+                    MenuSection = "@Simple Game Importer",
+                    Icon = Path.Combine(pluginFolder, "icon.png"),
+                    Description = resources.GetString("LOC_SIMPLEGAMEIMPORTER_Import"),
+                    Action = (gameMenuItem) =>
+                    {
+                       DoImportGames();                        
+                    }
+                }
+             };
+            return MainMenuItems;
+        }
+
+
+        public override void OnGameInstalled(Game game)
+        {
+            // Add code to be executed when game is finished installing.
+        }
+
+        public override void OnGameStarted(Game game)
+        {
+            // Add code to be executed when game is started running.
+        }
+
+        public override void OnGameStarting(Game game)
+        {
+            // Add code to be executed when game is preparing to be started.
+        }
+
+        public override void OnGameStopped(Game game, long elapsedSeconds)
+        {
+            // Add code to be executed when game is preparing to be started.
+        }
+
+        public override void OnGameUninstalled(Game game)
+        {
+            // Add code to be executed when game is uninstalled.
+        }
+
+        public override void OnApplicationStarted()
+        {
+            // Add code to be executed when Playnite is initialized.
+        }
+
+        public override void OnApplicationStopped()
+        {
+            // Add code to be executed when Playnite is shutting down.
+        }
+
+        public override void OnLibraryUpdated()
+        {
+            // Add code to be executed when library is updated.
+        }
+
+        public override ISettings GetSettings(bool firstRunSettings)
+        {
+            return Settings;
+        }
+
+        public override UserControl GetSettingsView(bool firstRunSettings)
+        {
+            return new ImportCSVSettingsView();
+        }
+    }
+}
